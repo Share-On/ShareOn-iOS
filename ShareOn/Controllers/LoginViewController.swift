19d75e658fd8c74b168d405ba757ec027167be99
YeonJu <yjnoh1016@naver.com>
2021-10-08 15:59:41 +0900

    [INIT] Font and Group

---
+++ ShareOn/Controllers/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  ShareOn
+//
+//  Created by 노연주 on 2021/10/08.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class LoginViewController: UIViewController {
+    //MARK: - Properties
+    
+    //MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+        
+    //MARK: - Selectors
+
+    
+    //MARK: - Helpers
+    func configureUI(){
+        view.backgroundColor = .white
+        addView()
+        cornerRadius()
+        location()
+    }
+    
+    func addView(){
+        
+    }
+    
+    func cornerRadius(){
+        
+    }
+    
+    func location(){
+        
+    }
+}
