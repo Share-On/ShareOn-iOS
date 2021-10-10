648f367bd039702a527e7eeecef31ffa1f5686a1
노연주 <yjnoh1016@naver.com>
2021-10-10 14:56:37 +0900

    [UPDATE] Setting

--- ShareOn/Controllers/SignUpUserViewController.swift
+++ ShareOn/Controllers/SignUpUserViewController.swift
@@ -6,12 +6,69 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class SignUpUserViewController: UIViewController {
+    //MARK: - Properties
     
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+    }
+    
+    //MARK: - Selectors
+    
+    //MARK: - Helpers
+    private func configureUI(){
+        view.backgroundColor = .white
+        addView()
+        cornerRadius()
+        location()
+    }
+    
+    // MARK: - Add View
+    
+    private func addView(){
+        
+    }
+    
+    // MARK: - Corner Radius
+    
+    private func cornerRadius(){
+        
+    }
+    
+    // MARK: - Location
+    
+    private func location(){
+        
     }
     
 }
+
+//MARK: - Preview
+#if DEBUG
+import SwiftUI
+struct SignUpUserViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        SignUpUserViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct SignUpUserViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            SignUpUserViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
