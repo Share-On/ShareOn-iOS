a9f93586761c2428f1ffaff935ecd1d833a4be9f
노연주 <yjnoh1016@naver.com>
2021-10-10 12:33:29 +0900

    [UPDATE] Add titleLabel

--- ShareOn/Controllers/LoginViewController.swift
+++ ShareOn/Controllers/LoginViewController.swift
@@ -12,6 +12,12 @@ import Then
 class LoginViewController: UIViewController {
     //MARK: - Properties
     
+    private let titleLabel = UILabel().then {
+        $0.text = "LogIn"
+        $0.dynamicFont(fontSize: 30, currentFontName: "AlfaSlabOne-Regular")
+        $0.textColor = .rgb(red: 255, green: 177, blue: 197)
+    }
+    
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +36,7 @@ class LoginViewController: UIViewController {
     }
     
     func addView(){
-        
+        view.addSubview(titleLabel)
     }
     
     func cornerRadius(){
@@ -38,6 +44,35 @@ class LoginViewController: UIViewController {
     }
     
     func location(){
-        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(self.view.frame.height/4.75)
+            make.left.equalToSuperview().offset(self.view.frame.width/6.25)
+        }
     }
 }
+
+//MARK: - Preview
+#if DEBUG
+import SwiftUI
+struct LoginViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        LoginViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct LoginViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            LoginViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
