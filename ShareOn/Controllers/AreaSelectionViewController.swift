451dff6b1ba62ca96b83ad8948f87d348039d9c6
노연주 <yjnoh1016@naver.com>
2021-10-11 15:09:15 +0900

    [UPDATE] Add area

--- ShareOn/Controllers/AreaSelectionViewController.swift
+++ ShareOn/Controllers/AreaSelectionViewController.swift
@@ -140,6 +140,24 @@ class AreaSelectionViewController: UIViewController {
             secondDropDown.dataSource = ["동구", "중구", "서구", "유성구", "대덕구"]
         } else if firstAreaLabel.text == "울산광역시" {
             secondDropDown.dataSource = ["중구", "남구", "동구", "북구", "울주군"]
+        } else if firstAreaLabel.text == "경기도" {
+            secondDropDown.dataSource = ["수원시", "고양시", "용인시", "성남시", "화성시", "부천시", "남양주시", "안산시", "안양시", "평택시", "시흥시", "김포시", "파주시", "의정부시", "광주시", "하남시", "광명시", "군포시", "양주시", "구리시", "오산시", "안성시", "의왕시", "이천시", "포천시", "동두천시", "과천시", "여주시", "양평군", "가평군", "연천군"]
+        } else if firstAreaLabel.text == "강원도" {
+            secondDropDown.dataSource = ["춘천시", "원주시", "강릉시", "동해시", "태백시", "속초시", "삼척시", "홍천군", "횡성군", "영월군", "평창군", "정선군", "철원군", "화천군", "양구군", "인제군", "고성군", "양양군"]
+        } else if firstAreaLabel.text == "경상북도" {
+            secondDropDown.dataSource = ["포항시", "경주시", "김천시", "안동시", "구미시", "영주시", "영천시", "상주시", "문경시", "경산시", "군위군", "의성군", "청송군", "영양군", "영덕군", "청도군", "고령군", "성주군", "칠곡군", "예천군", "봉화군", "울진군", "울릉군"]
+        } else if firstAreaLabel.text == "경상남도" {
+            secondDropDown.dataSource = ["창원시", "김해시", "양산시", "진주시", "거제시", "통영시", "사천시", "밀양시", "함안군", "거창군", "창녕군", "고성군", "하동군", "합천군", "남해군", "함양군", "산청군", "의령군"]
+        } else if firstAreaLabel.text == "충청북도" {
+            secondDropDown.dataSource = ["청주시", "충주시", "제천시", "보은군", "옥천군", "영동군", "증평군", "진천군", "괴산군", "음성군", "단양군"]
+        } else if firstAreaLabel.text == "충청남도" {
+            secondDropDown.dataSource = ["천안시", "공주시", "보령시", "아산시", "서산시", "논산시", "계룡시", "당진시", "금산군", "부여군", "서천군", "청양군", "홍성군", "예산군", "태안군"]
+        } else if firstAreaLabel.text == "전라북도" {
+            secondDropDown.dataSource = ["전주시", "익산시", "군산시", "정읍시", "남원시", "김제시", "완주군", "고창군", "부안군", "임실군", "순창군", "진안군", "무주군", "장수군"]
+        } else if firstAreaLabel.text == "전라남도" {
+            secondDropDown.dataSource = ["목포시", "여수시", "순천시", "나주시", "광양시", "담양군", "곡성군", "구례군", "고흥군", "보성군", "화순군", "장흥군", "강진군", "해남군", "영암군", "무안군", "함평군", "영광군", "장성군", "완도군", "진도군", "신안군"]
+        } else if firstAreaLabel.text == "제주특별자치도" {
+            secondDropDown.dataSource = ["제주시", "서귀포시"]
         }
     }
     
