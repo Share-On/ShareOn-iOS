eaa53ad72618f891c5698594a4e12c7a2846a190
노연주 <yjnoh1016@naver.com>
2021-10-12 20:27:34 +0900

    [UPDATE] Add deviceInfoView

--- ShareOn/Controllers/DeviceLinkageViewController.swift
+++ ShareOn/Controllers/DeviceLinkageViewController.swift
@@ -12,10 +12,16 @@ import Then
 class DeviceLinkageViewController: UIViewController {
     //MARK: - Properties
     
+    lazy var deviceName:String = "ShareOn_Ec0929"
+    
     private let titleLabel = UILabel().then {
         $0.text = "Device linkage"
         $0.dynamicFont(fontSize: 30, currentFontName: "AlfaSlabOne-Regular")
         $0.textColor = .rgb(red: 159, green: 196, blue: 231)
+    }
+    
+    lazy var deviceInfoView = DeviceView().then {
+        $0.name.text = deviceName
     }
     
     //MARK: - Lifecycle
@@ -29,6 +35,7 @@ class DeviceLinkageViewController: UIViewController {
     //MARK: - Helpers
     private func configureUI(){
         view.backgroundColor = .white
+        deviceViewSetting()
         addView()
         cornerRadius()
         location()
@@ -38,12 +45,16 @@ class DeviceLinkageViewController: UIViewController {
     
     private func addView(){
         view.addSubview(titleLabel)
+        view.addSubview(deviceInfoView)
     }
     
     // MARK: - Corner Radius
     
     private func cornerRadius(){
-        
+        deviceInfoView.layer.borderWidth = 1
+        deviceInfoView.layer.borderColor = UIColor.rgb(red: 229, green: 229, blue: 229).cgColor
+        deviceInfoView.layer.cornerRadius = self.view.frame.width/15
+        deviceInfoView.deviceImage.layer.cornerRadius = self.view.frame.width/5
     }
     
     // MARK: - Location
@@ -53,6 +64,21 @@ class DeviceLinkageViewController: UIViewController {
             make.top.equalToSuperview().offset(self.view.frame.height/5.07)
             make.left.equalToSuperview().offset(self.view.frame.width/6.25)
         }
+        
+        deviceInfoView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(titleLabel.snp.bottom).offset(self.view.frame.height/17.28)
+            make.width.equalToSuperview().dividedBy(1.15)
+            make.height.equalToSuperview().dividedBy(2.42)
+        }
+    }
+    
+    private func deviceViewSetting(){
+        deviceInfoView.addSubview(deviceInfoView.backView)
+        deviceInfoView.addSubview(deviceInfoView.name)
+        deviceInfoView.addSubview(deviceInfoView.deviceImage)
+        
+        deviceInfoView.deviceViewSetting(screenHeight: self.view.frame.height, screenWidth: self.view.frame.width)
     }
     
 }
