19055fc2ef653847843c0e7d81fb53d987172dd5
노연주 <yjnoh1016@naver.com>
2021-10-30 14:37:39 +0900

    [UPDATE] Add PinkTabBar Func

--- ShareOn/Views/TabBar.swift
+++ ShareOn/Views/TabBar.swift
@@ -152,4 +152,47 @@ class TabBar: UIView {
             make.height.equalTo(screenHeight/23.2)
         }
     }
+    
+    func pinkTabBarSetting(screenHeight: CGFloat, screenWidth: CGFloat){
+        backgroundImage.image = UIImage(named: "ShareOn-PinkBack")
+        mainButton.setBackgroundImage(UIImage(named: "ShareOn-PinkMain"), for: .normal)
+        locationButton.setBackgroundImage(UIImage(named: "ShareOn-PinkLocation"), for: .normal)
+        historyButton.setBackgroundImage(UIImage(named: "ShareOn-PinkHistory"), for: .normal)
+        settingButton.setBackgroundImage(UIImage(named: "ShareOn-PinkSetting"), for: .normal)
+        
+        backgroundImage.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.left.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalToSuperview()
+        }
+        
+        mainButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(screenHeight/67.67)
+            make.left.equalTo(backgroundImage).offset(screenWidth/12.5)
+            make.width.equalTo(screenWidth/12.10)
+            make.height.equalTo(screenHeight/23.88)
+        }
+        
+        locationButton.snp.makeConstraints { make in
+            make.centerY.equalTo(mainButton)
+            make.left.equalTo(backgroundImage).offset(screenWidth/3.02)
+            make.width.equalTo(screenWidth/15)
+            make.height.equalTo(screenHeight/25.38)
+        }
+        
+        historyButton.snp.makeConstraints { make in
+            make.centerY.equalTo(mainButton)
+            make.left.equalTo(backgroundImage).offset(screenWidth/1.63)
+            make.width.equalTo(screenWidth/15)
+            make.height.equalTo(screenHeight/32.48)
+        }
+        
+        settingButton.snp.makeConstraints { make in
+            make.centerY.equalTo(mainButton)
+            make.left.equalTo(backgroundImage).offset(screenWidth/1.17)
+            make.width.equalTo(screenWidth/10.71)
+            make.height.equalTo(screenHeight/23.2)
+        }
+    }
 }
