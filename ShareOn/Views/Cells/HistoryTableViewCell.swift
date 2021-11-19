938bcce77f0c7f87d9afe44a79642a92b3cf772e
노연주 <yjnoh1016@naver.com>
2021-11-19 14:58:30 +0900

    [UPDATE] Add Property

--- ShareOn/Views/Cells/HistoryTableViewCell.swift
+++ ShareOn/Views/Cells/HistoryTableViewCell.swift
@@ -6,18 +6,51 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class HistoryTableViewCell: UITableViewCell {
+    
+    static let HistoryTableViewCellIdentifier = "\(HistoryTableViewCell.self)"
 
+    let plusOrMinus = UILabel().then {
+        $0.text = "+"
+        $0.dynamicFont(fontSize: 38, currentFontName: "AppleSDGothicNeo-Regular")
+        $0.textColor = .plusColor
+    }
+    
+    let dateLabel = UILabel().then {
+        $0.text = "2021년 9월 29일"
+        $0.textColor = .rgba(red: 0, green: 0, blue: 0, alpha: 0.24)
+        $0.dynamicFont(fontSize: 10, currentFontName: "AppleSDGothicNeo-Regular")
+    }
+    
+    let locationLabel = UILabel().then {
+        $0.text = "광주광역시 광산구"
+        $0.dynamicFont(fontSize: 14, currentFontName: "AppleSDGothicNeo-Thin")
+    }
+    
+    let energyLabel = UILabel().then {
+        $0.text = "230kwh"
+        $0.textColor = .plusEnergyColor
+        $0.dynamicFont(fontSize: 16, currentFontName: "AlfaSlabOne-Regular")
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureUI()
+    }
+       
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureUI()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configureUI(){
+        
     }
-
 }
