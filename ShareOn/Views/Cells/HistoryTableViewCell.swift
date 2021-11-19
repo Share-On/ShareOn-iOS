05093be38ef7ae7a9a0fd3a748eff422c0901d63
노연주 <yjnoh1016@naver.com>
2021-11-19 10:31:19 +0900

    [CREATE] HistoryTableViewCell

---
+++ ShareOn/Views/Cells/HistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HistoryTableViewCell.swift
+//  ShareOn
+//
+//  Created by 노연주 on 2021/11/19.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
