a73182b7463ba0050a8635db807261cbf68a9b7c
노연주 <yjnoh1016@naver.com>
2021-11-20 21:50:31 +0900

    [CREATE] PlusEnergyView

---
+++ ShareOn/Views/Cells/PlusEnergyAlterView.swift
@@ -0,0 +1,20 @@
+//
+//  PlusEnergyAlterView.swift
+//  ShareOn
+//
+//  Created by 노연주 on 2021/11/20.
+//
+
+import UIKit
+
+class PlusEnergyAlterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
