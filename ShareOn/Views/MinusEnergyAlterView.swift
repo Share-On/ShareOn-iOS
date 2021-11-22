7d18d0d9383171610fac247f9a3d1f38b0009606
노연주 <yjnoh1016@naver.com>
2021-11-23 01:33:36 +0900

    [UPDATE] MinusEnergyAlterView Complete

--- ShareOn/Views/MinusEnergyAlterView.swift
+++ ShareOn/Views/MinusEnergyAlterView.swift
@@ -8,13 +8,48 @@
 import UIKit
 
 class MinusEnergyAlterView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    lazy var energyTf = UITextField().then {
+        $0.backgroundColor = .rgb(red: 211, green: 229, blue: 246)
+        $0.layer.cornerRadius = 15
+        $0.dynamicFont(fontSize: 20, currentFontName: "AlfaSlabOne-Regular")
+        $0.textColor = .rgb(red: 70, green: 150, blue: 225)
+        $0.addLeftPadding()
     }
-    */
-
+    
+    lazy var cancleButton = UIButton().then {
+        $0.setTitle("취소", for: .normal)
+        $0.backgroundColor = .rgb(red: 223, green: 223, blue: 223)
+        $0.dynamicFont(fontSize: 11, currentFontName: "AppleSDGothicNeo-Medium")
+        $0.layer.cornerRadius = 10
+    }
+    
+    lazy var okButton = UIButton().then {
+        $0.setTitle("확인", for: .normal)
+        $0.setTitleColor(.rgb(red: 70, green: 150, blue: 225), for: .normal)
+        $0.backgroundColor = .rgb(red: 211, green: 229, blue: 246)
+        $0.dynamicFont(fontSize: 11, currentFontName: "AppleSDGothicNeo-Medium")
+        $0.layer.cornerRadius = 10
+    }
+    
+    func layoutSetting(sw: CGFloat, sh: CGFloat){
+        energyTf.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(sh/38.67)
+            make.width.equalTo(sw/1.41)
+            make.height.equalTo(sh/14.25)
+        }
+        
+        cancleButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(sh/45.11)
+            make.left.equalToSuperview().offset(sw/2.68)
+            make.width.equalTo(sw/5.36)
+            make.height.equalTo(sh/29)
+        }
+        
+        okButton.snp.makeConstraints { make in
+            make.top.equalTo(cancleButton)
+            make.left.equalTo(cancleButton.snp.right).offset(23.44)
+            make.width.height.equalTo(cancleButton)
+        }
+    }
 }
