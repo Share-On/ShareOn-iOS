cb759be73eaeca49cde428748671d0e1b89a9ab0
노연주 <yjnoh1016@naver.com>
2021-11-24 20:19:03 +0900

    [UPDATE] Finish Contest

--- ShareOn/Resources/SceneDelegate.swift
+++ ShareOn/Resources/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let mainViewController = LoginViewController()
+        let mainViewController = MainViewController()
         let navigationController = UINavigationController(rootViewController: mainViewController)
         navigationController.navigationBar.isHidden = true
         window?.rootViewController = navigationController
